f11e4699dd121f7d26b0869fbe89f640d71bc925
Flora Afroza <afzflora@gmail.com>
2023-05-04 17:17:01 -0400

    function algorithms

---
+++ timetravel.lisp
@@ -0,0 +1,16 @@
+
+;; 2-way Universe Attributes
+
+
+;; Reference to current event
+(defvar currentEvent 0)
+
+;; 2-way event Timeline indexing
+(defun binarySearch x)
+
+;; Add events to 2-way Timeline
+(defun binarySort x)
+
+;; Choices of 2-way actions to be indexed via Binary Search
+
+;; Corresponding props and aspects of each event via index
